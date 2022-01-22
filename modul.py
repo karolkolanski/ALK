c3a3e9297321e37aec4a8132a9254a271bafe23f
Karol Kolanski <gralu86@gmail.com>
2022-01-22 10:35:08 +0100

    Modul and program added

---
+++ modul.py
@@ -0,0 +1,6 @@
+print("MODUŁ WITA!")
+print("plik __name__ =", __name__)
+
+class Hack:
+    def hack_site(self, site):
+        print("Łamię hasło do strony", site)
---
+++ program.py
@@ -0,0 +1,6 @@
+import modul
+print("plik __name__ =", __name__)
+
+instacja = modul.Hack()
+
+instacja.hack_site("www.wp.pl")
