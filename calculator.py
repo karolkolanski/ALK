866042978a6a636982a0d47f7ac4412f01185ef7
Karol Kolanski <gralu86@gmail.com>
2022-01-22 12:30:43 +0100

    add_etended added in Calculator

--- calculator.py
+++ calculator.py
@@ -1,3 +1,8 @@
 class Calculator:
     def add(self, a, b):
         return a + b
+    def add_extended(self, *args):
+        result = 0
+        for arg in args:
+            result += arg
+        return result
--- program.py
+++ program.py
@@ -2,4 +2,8 @@ import calculator
 
 calc = calculator.Calculator()
 result = calc.add(4,6)
+
 print(result)
+
+result_extended= calc.add_extended(1,2,3)
+print(result_extended)
